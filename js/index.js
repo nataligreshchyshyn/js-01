e413f22a1c461280c181db6d500bf5a1433fa996
Natali Hreshchyshyn <natalihreshchyshyn@gmail.com>
2018-05-30 15:59:08 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,40 @@
+//const userName = prompt('Enter your name!');
+//const greet = 'Hello ' + userName;
+//alert(greet);
+
+//const userAge = parseInt(prompt('Enter your age, please!'))
+//const nextyear = userAge + 1
+//alert(nextyear);
+
+//if (userAge < 18) {
+//    alert('Child');
+//} else if  (userAge < 80) {
+//    alert('Adult');
+//} else {
+//    alert('Old');
+//}
+
+//const message = 'Вітаємо на нашому сайті!';
+//const greetings = 'Привіт!';
+//alert(greetings);
+//alert(message);
+
+function sum(a, b) {
+    const result = a + b;
+    return result;
+}
+
+let sumOfNums = sum(10, 20);
+alert(sumOfNums);
+sumOfNums = sum(20, 30);
+alert(sumOfNums);
+
+function sum3(a, b, c){
+    const result = a+b+c
+    return result;
+}
+
+let sumOf3Nums = sum3(15, 16, 14);
+alert(sumOf3Nums);
+sumOf3Nums = sum3(46, 26, 13);
+alert(sumOf3Nums);
